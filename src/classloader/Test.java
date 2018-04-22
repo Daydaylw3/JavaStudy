0fed71914df448675c6ac7a669b712a91d47cec3
谭乐天 <daydaylw3@2980.com>
2018-04-22 13:14:19 +0800

    ClassLoader & controlflow
    如上

---
+++ src/classloader/Test.java
@@ -0,0 +1,9 @@
+package classloader;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("sun.boot.class.path"));
+		System.out.println(System.getProperty("java.ext.dirs"));
+		System.out.println(System.getProperty("java.class.path"));
+	}
+}
