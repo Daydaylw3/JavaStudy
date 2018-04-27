e7f261d24e96a66d0aa939a3d59309320cbf23fc
Daydaylw3 <daydalw3@2980.com>
2018-04-28 01:26:16 +0800

    修改了礼包类
    修改了礼包类

--- src/study/CalculateMoney.java
+++ src/study/CalculateMoney.java
@@ -2,22 +2,32 @@ package study;
 
 import java.util.ArrayList;
 
-public class CalculateMoney {
 
+/**
+ * 这个类是为了实现这样的需求：
+ * 礼包1：充值rmb 256 得 1875金；礼包2：充值rmb 128 得 813金；
+ * 礼包3：充值rmb 68 得375金；礼包4：充值rmb 30 得163金
+ * 若想得到 2400金，则最省钱的情况下要花多少 rmb ??? 如何搭配礼包的购买???
+ * 
+ * 20180427 by tanlt
+ * 暂时只实现了穷举法
+ * */
+public class CalculateMoney {
+	public static final int U = 1875;
+	public static final int X = 813;
+	public static final int Y = 375;
+	public static final int Z = 163;
+	
 	public static void main(String[] args) {
 		int u = 0, x = 0, y = 0, z = 14;
 		ArrayList<Calcu> c = new ArrayList<Calcu>();
 		CalculateMoney calcu = new CalculateMoney();
-//		Calcu ca = new CalculateMoney().new Calcu();
-		for(; 1875 * u + 813 * x + 375 * y + 163 * z < 2563; u++) {
-			for (; 1875 * u + 813 * x + 375 * y + 163 * z < 2563; x++) {
-				for(; 1875 * u + 813 * x + 375 * y + 163 * z < 2563; y ++) {
-					for(; 1875 * u + 813 * x + 375 * y + 163 * z < 2563; z ++) {
-//					System.out.println("x = " + x + ", y = " + y + ", z = " + z + ", total = " + (813 * x + 375 * y + 163 * z));
-						if(1875 * u + 813 * x + 375 * y + 163 * z < 2563 && 813 * x + 375 * y + 163 * z >= 2400) {
-//						System.out.println("x = " + x + ", y = " + y + ", z = " + z +
-//								", total = " + (813 * x + 375 * y + 163 * z) + ", rmb = " + (128 * x + 68 * y + 30 * z));
-							Calcu ca = calcu.new Calcu(u, x, y, z, (1875 * u + 813 * x + 375 * y + 163 * z),  (258 * u + 128 * x + 68 * y + 30 * z));
+		for(; U * u + X * x + Y * y + Z * z < (Z + 2400); u++) {
+			for (; U * u + X * x + Y * y + Z * z <  (Z + 2400); x++) {
+				for(; U * u + X * x + Y * y + Z * z <  (Z + 2400); y ++) {
+					for(; U * u + X * x + Y * y + Z * z <  (Z + 2400); z ++) {
+						if((U * u + X * x + Y * y + Z * z <  (Z + 2400)) && (U * u + X * x + Y * y + Z * z >= 2400)) {
+							Calcu ca = calcu.new Calcu(u, x, y, z, (U * u + X * x + Y * y + 163 * z),  (258 * u + 128 * x + 68 * y + 30 * z));
 							c.add(ca);
 						}
 					}
@@ -32,7 +42,12 @@ public class CalculateMoney {
 		}
 		calcu.sort(c);
 		for(Calcu c1 : c) {
-			System.out.println("rmb = " + c1.getRmb() + ", 258元礼包 = " + c1.getU() + ", 128元礼包 = " + c1.getX() + ", 68元礼包 = " + c1.getY() + ", 30元礼包 = " + c1.getZ() + ", gold = " + c1.getGold());
+			System.out.println("rmb = " + c1.getRmb() + 
+					(c1.getU() > 0 ? (", 258元礼包 = " + c1.getU()) : "") + 
+					(c1.getX() > 0 ? (", 128元礼包 = " + c1.getX()) : "") + 
+					(c1.getY() > 0 ? (", 68元礼包 = " + c1.getY()) : "") + 
+					(c1.getZ() > 0 ? (", 30元礼包 = " + c1.getZ()) : "") + 
+					", gold = " + c1.getGold());
 		}
 	}
 	
