1495201ca6f702ddf97eba70b3ce042aaf4473b2
Daydaylw3 <daydalw3@2980.com>
2018-05-02 03:12:12 +0800

    礼包类
    修改了部分代码

--- src/shenwu/CalculateMoney.java
+++ src/shenwu/CalculateMoney.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
  * 
  * 20180427 by tanlt
  * 暂时只实现了穷举法
+ * 
+ * 20180502 by tanlt
+ * 期待实现限时数量
  * */
 public class CalculateMoney {
 	
@@ -29,10 +32,19 @@ public class CalculateMoney {
 	public static final int FGet = 62;
 	
 	
-	
+	/*
+	 * for语句的顺序:
+	 * for(state1; state2; state3){
+	 * 		state4;
+	 * }
+	 * 1、先state1
+	 * 2、判断state2
+	 * 3、为假跳出for循环；为真state4，然后state3
+	 * 4、然后2、3步骤循环直至state2为假
+	 * */
 	public static void count(int target) {
 		int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
-		ArrayList<Calcu> cl = new ArrayList<Calcu>();
+		ArrayList<GiftBag> cl = new ArrayList<GiftBag>();
 		CalculateMoney calcu = new CalculateMoney();
 		for (; AGet * a + BGet * b + CGet * c + DGet * d + EGet * e  + FGet * f < (FGet + target); a++) {
 			int tempA = AGet * a;
@@ -47,7 +59,7 @@ public class CalculateMoney {
 							for(; tempE + FGet * f < (FGet + target); f++) {
 								if(tempE + FGet * f >= target) {
 									//符合要求
-									Calcu ca = calcu.new Calcu(a, b, c, d, e, f, tempE + FGet * f, 
+									GiftBag ca = calcu.new GiftBag(a, b, c, d, e, f, tempE + FGet * f, 
 											ACost * a + BCost * b + CCost * c + DCost * d + ECost * e  + FCost * f);
 									cl.add(ca);
 								}
@@ -64,33 +76,39 @@ public class CalculateMoney {
 		}
 		
 		calcu.sort(cl);
-		for(Calcu c1 : cl) {
-			System.out.println("花费:" + c1.getRmb() + "元" + 
-					", 购买礼包数:" + c1.getAll() + 
-					(c1.getA() > 0 ? (", 648元礼包=" + c1.getA()) : "") + 
-					(c1.getB() > 0 ? (", 258元礼包=" + c1.getB()) : "") + 
-					(c1.getC() > 0 ? (", 128元礼包=" + c1.getC()) : "") + 
-					(c1.getD() > 0 ? (", 68元礼包=" + c1.getD()) : "") + 
-					(c1.getE() > 0 ? (", 30元礼包=" + c1.getE()) : "") + 
-					(c1.getF() > 0 ? (", 12元礼包=" + c1.getF()) : "") +
-					", gold = " + c1.getGold());
+		for(GiftBag c1 : cl) {
+			if(c1.getAll() < 7)
+				System.out.println("花费:" + c1.getRmb() + "元" + 
+						", 购买礼包数:" + c1.getAll() + 
+						(c1.getA() > 0 ? (", 648元礼包=" + c1.getA()) : "") + 
+						(c1.getB() > 0 ? (", 258元礼包=" + c1.getB()) : "") + 
+						(c1.getC() > 0 ? (", 128元礼包=" + c1.getC()) : "") + 
+						(c1.getD() > 0 ? (", 68元礼包=" + c1.getD()) : "") + 
+						(c1.getE() > 0 ? (", 30元礼包=" + c1.getE()) : "") + 
+						(c1.getF() > 0 ? (", 12元礼包=" + c1.getF()) : "") +
+						", gold = " + c1.getGold());
 		}
 	}
 	
-	public static void main(String[] args) {
-		count(998);
+	public static void count(int target, int limitTimes) {
+		
 	}
 	
-	public void sort(ArrayList<Calcu> array) {
+	public static void main(String[] args) {
+		count(4000);
+	}
+	
+	public void sort(ArrayList<GiftBag> array) {
 		if(array.size() == 0)
 			return ;
 		for(int i = 0; i < array.size(); i ++) {
 			for(int j = 0; j < array.size() - i - 1; j ++) {
+				//这里是比较因子
 				double count1 = 1.5 * array.get(j).getRmb() + 23.5 * array.get(j).getAll();
 				double count2 = 1.5 * array.get(j + 1).getRmb() + 23.5 * array.get(j + 1).getA();
 				if (count1 > count2) {
-					Calcu temp1 = array.get(j);
-					Calcu temp2 = array.get(j + 1);
+					GiftBag temp1 = array.get(j);
+					GiftBag temp2 = array.get(j + 1);
 					array.set(j, temp2);
 					array.set(j + 1, temp1);
 				}
@@ -98,77 +116,49 @@ public class CalculateMoney {
 		}
 	}
 	
-	public class Calcu{
-		private int a;
-		private int b;
-		private int c;
-		private int d;
-		private int e;
-		private int f;
+	public class GiftBag{
+		private int a, b, c, d, e, f;
 		private int gold;
 		private int rmb;
 		
-		Calcu(int a, int b, int c, int d, int e, int f, int gold, int rmb){
-			this.a = a;
-			this.b = b;
-			this.c = c;
-			this.d = d;
-			this.e = e;
-			this.f = f;
+		GiftBag(int a, int b, int c, int d, int e, int f, int gold, int rmb){
+			this.a = a; this.b = b; this.c = c; this.d = d; this.e = e; this.f = f;
 			this.gold = gold;
 			this.rmb = rmb;
 		}
-		public int getAll() {
-			return a + b + c + d + e + f;
-		}
-		public int getA() {
-			return a;
-		}
-		public void setA(int a) {
-			this.a = a;
-		}
-		public int getB() {
-			return b;
-		}
-		public void setB(int b) {
-			this.b = b;
-		}
-		public int getC() {
-			return c;
-		}
-		public void setC(int c) {
-			this.c = c;
-		}
-		public int getD() {
-			return d;
-		}
-		public void setD(int d) {
-			this.d = d;
-		}
-		public int getE() {
-			return e;
-		}
-		public void setE(int e) {
-			this.e = e;
-		}
-		public int getF() {
-			return f;
-		}
-		public void setF(int f) {
-			this.f = f;
-		}
-		public int getGold() {
-			return gold;
-		}
-		public void setGold(int gold) {
-			this.gold = gold;
-		}
-		public int getRmb() {
-			return rmb;
-		}
-		public void setRmb(int rmb) {
-			this.rmb = rmb;
-		}
+		public int getAll() { return a + b + c + d + e + f; }
+		
+		public int getA() { return a; }
+		
+		public void setA(int a) { this.a = a; }
+		
+		public int getB() { return b; }
+		
+		public void setB(int b) { this.b = b; }
+		
+		public int getC() { return c; }
+		
+		public void setC(int c) { this.c = c; }
+		
+		public int getD() { return d; }
+		
+		public void setD(int d) { this.d = d; }
+		
+		public int getE() { return e; }
+		
+		public void setE(int e) { this.e = e; }
+		
+		public int getF() { return f; }
+		
+		public void setF(int f) { this.f = f; }
+		
+		public int getGold() { return gold; }
+		
+		public void setGold(int gold) { this.gold = gold; }
+		
+		public int getRmb() { return rmb; }
+		
+		public void setRmb(int rmb) { this.rmb = rmb; }
 	}
 
 }
