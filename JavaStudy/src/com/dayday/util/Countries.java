5c0851de079dfb034f2c37aec6f24067022e16bb
Daydaylw3 <38597251+Daydaylw3@users.noreply.github.com>
2018-09-03 00:55:21 +0800

    容器

---
+++ JavaStudy/src/com/dayday/util/Countries.java
@@ -0,0 +1,111 @@
+package com.dayday.util;
+
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/*
+ * 17.2.3 使用Abstract类
+ * */
+public class Countries {
+	public static final String[][] DATA = {
+			{"ALGERIA", "Algiers"},	{"ANGOLA", "Luanda"},
+			{"CHINA", "Beijing"}, {"ARGENTINA", "Buenos Aires"},
+	};
+	
+	//Use AbstractMap by implementing entrySet()
+	private static class FlyweightMap
+	extends AbstractMap<String, String>{
+		private static class Entry
+		implements Map.Entry<String, String>{
+			int index;
+			Entry(int index) { this.index = index; }
+			public boolean equals(Object o) {
+				return DATA[index][0].equals(o);
+			}
+			public String getKey() {
+				return DATA[index][0];
+			}
+			public String getValue() {
+				return DATA[index][1];
+			}
+			public String setValue(String value) {
+				throw new UnsupportedOperationException();
+			}
+			public int hashCode() {
+				return DATA[index][0].hashCode();
+			}
+		}
+		// Use AbstractSet by implementing size() & iterator()
+		static class EntrySet
+		extends AbstractSet<Map.Entry<String, String>> {
+			private int size;
+			EntrySet(int size){
+				if(size < 0)
+					this.size = 0;
+				else if(size > DATA.length)
+					this.size = DATA.length;
+				else
+					this.size = size;
+			}
+			private class Iter
+			implements Iterator<Map.Entry<String, String>>{
+				private Entry entry = new Entry(-1);
+				public boolean hasNext() {
+					return entry.index < size - 1;
+				}
+				public void remove() {
+					throw new UnsupportedOperationException();
+				}
+				@Override
+				public Map.Entry<String, String> next() {
+					entry.index ++;
+					return entry;
+				}
+			}
+			@Override
+			public Iterator<Map.Entry<String, String>> iterator() {
+				return new Iter();
+			}
+
+			@Override
+			public int size() {
+				return size;
+			}
+		}
+		private static Set<Map.Entry<String, String>> entries =
+				new EntrySet(DATA.length);
+		@Override
+		public Set<Map.Entry<String, String>> entrySet() {
+			return entries;
+		}
+		
+	}
+	static Map<String, String> map = new FlyweightMap();
+	public static Map<String, String> select(final int size){
+		return new FlyweightMap() {
+			public Set<Map.Entry<String, String>> entrySet() {
+				return new EntrySet(size);
+			}
+		};
+	}
+	public static Map<String, String> capitals(int size){
+		return select(size);
+	}
+	static List<String> names =
+			new ArrayList<String>(map.keySet());
+	public static List<String> names() { return names; }
+	
+	public static List<String> names(int size) {
+		return new ArrayList<String>(select(size).keySet());
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(capitals(2));
+		System.out.println(names(1));
+	}
+}
--- JavaStudy/src/containers/MapDataTest.java
+++ JavaStudy/src/containers/MapDataTest.java
@@ -3,6 +3,7 @@ package containers;
 import java.util.Iterator;
 
 import com.dayday.util.Generator;
+import com.dayday.util.MapData;
 import com.dayday.util.Pair;
 
 
@@ -46,7 +47,10 @@ class Letters implements Generator<Pair<Integer, String>>,
 public class MapDataTest {
 
 	public static void main(String[] args) {
-		
+//		Pair Generator:
+		System.out.println(MapData.map(new Letters(), 11));
+//		an Iterable and a single value
+		System.out.println(MapData.map(new Letters(), "Pop"));
 	}
 
 }
