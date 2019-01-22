dbe63939dc37ca05311931d46ff2b1755133e61a
谭乐天 <daydaylw3@2980.com>
2019-01-22 11:15:21 +0800

    编码与解码

--- JavaStudy/src/fortest/CodeAndEncode.java
+++ JavaStudy/src/fortest/CodeAndEncode.java
@@ -4,6 +4,13 @@ public class CodeAndEncode {
 	public static void main(String[] args) {
 		String yan = "严";
 		byte[] byteYan = yan.getBytes();
+		System.out.println(byteYan);
+		for (byte c : byteYan) {
+			System.out.println(c);
+		}
+		byte[] t = {104,97,108,111};
+		String test = new String(t);
+		System.out.println(test);
 		byte b = 65;
 //		System.out.println(b);
 		String s = "helo";
@@ -11,10 +18,21 @@ public class CodeAndEncode {
 		for (byte c : helo) {
 			System.out.println(c);
 		}
-		char c = 'h';
+		char c = 'a';
 		helo[1] = (byte)c;
 		for (byte c1 : helo) {
 			System.out.println(c1);
 		}
+		System.out.println("-------");
+		char ccc = (char)1;
+		
+		helo[1] = (byte)ccc;
+		for (byte c1 : helo) {
+			System.out.println(c1);
+		}
+		char r = '匦';
+		byte rr = (byte)r;
+		System.out.println(rr);
+		System.out.println(Integer.toHexString(r));
 	}
 }
