d0e4e74c07faf20b4f8c2ca3edcc94c899084757
谭乐天 <daydaylw3@2980.com>
2019-02-03 11:25:09 +0800

    注解

--- JavaStudy/src/io/annotations/database/TableCreator.java
+++ JavaStudy/src/io/annotations/database/TableCreator.java
@@ -5,8 +5,21 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @ClassName io.annotations.database.TableCreator
+ * @Description 20.2.5 实现处理器<p>
+ * 下面是一个注解处理器的例子, 它将读取一个类文件,检查其上的数据库注解, 并生成用来创建数据库的SQL命令
+ * 
+ * @see io.annotations.database.DBTable
+ * @see io.annotations.database.Constraints
+ * @see io.annotations.database.SQLString
+ * @see io.annotations.database.SQLInteger
+ * 
+ * @author daydaylw3
+ * @date 2019年2月3日
+ */
 public class TableCreator {
-	
+	// 传参为 io.annotations.database.Member
 	public static void main(String[] args) throws Exception {
 		if (args.length < 1) {
 			System.out.println("arguements: annotated classes");
@@ -14,6 +27,7 @@ public class TableCreator {
 		}
 		for (String className : args) {
 			Class<?> cl = Class.forName(className);
+			// 检查是否带有@DBTable注解
 			DBTable dbTable = cl.getAnnotation(DBTable.class);
 			if (dbTable == null) {
 				System.out.println("no dbTable annotations in class " + className);
@@ -47,8 +61,15 @@ public class TableCreator {
 					} else {
 						columnName = sString.name();
 					}
-					columnDefs.add((columnName) + " INT" + getConstraints(sString.constraints()));
+					columnDefs.add((columnName) + " VARCHAR(" + sString.value() + ")" + getConstraints(sString.constraints()));
 				}
+				StringBuilder createCommand = new StringBuilder("CREATE TABLE " + tableName + "(");
+				for (String columnDef : columnDefs) {
+					createCommand.append("\n   " + columnDef + ",");
+				}
+				// remove trailing comma
+				String tableCreate = createCommand.substring(0, createCommand.length() - 1) + ");";
+				System.out.println("Table Creation SQL for " + className + " is :\n" + tableCreate);
 			}
 		}
 	}
