33159628aa5dbbebcfdaeff6a0910323e541b9bd
Daydaylw3 <38597251+Daydaylw3@users.noreply.github.com>
2019-02-18 00:34:55 +0800

    concurrency

--- JavaStudy/src/concurrency/DaemonThreadFactory.java
+++ JavaStudy/src/concurrency/DaemonThreadFactory.java
@@ -2,8 +2,18 @@ package concurrency;
 
 import java.util.concurrent.ThreadFactory;
 
+/**
+ * @ClassName concurrency.DaemonThreadFactory
+ * @Description 21.2.8 后台线程<p>
+ * @Todo 用Mac补上注释
+ * 
+ * @see concurrency.DaemonFromFactory
+ * 
+ * @author dayday
+ * @date 2019年2月17日
+ */
 public class DaemonThreadFactory implements ThreadFactory {
-
+	
 	@Override
 	public Thread newThread(Runnable r) {
 		Thread t = new Thread(r);
