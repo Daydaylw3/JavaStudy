5f05e294a5991ca3579b8d45d0eabae63b97e2b2
Daydaylw3 <38597251+Daydaylw3@users.noreply.github.com>
2019-02-19 00:19:19 +0800

    CONCURRENCY

---
+++ JavaStudy/src/concurrency/DaemonsDontRunFinally.java
@@ -0,0 +1,25 @@
+package concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName concurrency.DaemonsDontRunFinally
+ * @Description 21.2.8 后台线程<p>
+ * @Todo 用Mac补上注释
+ * 
+ * @author dayday
+ * @date 2019年2月18日
+ */
+public class DaemonsDontRunFinally {
+	
+}
+
+class ADaemon implements Runnable {
+	@Override
+	public void run() {
+//		try {
+//			System.out.println("starting adaemon");
+//			TimeUnit.
+//		}
+	}
+}
