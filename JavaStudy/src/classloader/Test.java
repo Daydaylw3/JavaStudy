855890255b3dffb803516fdd3a4f8bdd34a31e2d
Daydaylw3 <38597251+Daydaylw3@users.noreply.github.com>
2019-03-02 15:40:18 +0800

    111

--- JavaStudy/src/classloader/Test.java
+++ JavaStudy/src/classloader/Test.java
@@ -1,9 +1,29 @@
 package classloader;
 
+/**
+ * @ClassName classloader.Test
+ * @Description 
+ * 
+ * @see ClassLoader#loadClass(String)
+ * @see ClassLoader#loadClass(String, boolean)
+ * 
+ * @author dayday
+ * @date before 2019年3月2日
+ */
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		System.out.println(System.getProperty("sun.boot.class.path"));
 		System.out.println(System.getProperty("java.ext.dirs"));
 		System.out.println(System.getProperty("java.class.path"));
+		
+		System.out.println(" --- 2019.3.2 --- ");
+		ClassLoader classLoader = Test.class.getClassLoader();
+		System.out.println(classLoader);
+		System.out.println(classLoader.getParent());
+		System.out.println(classLoader.getParent().getParent());
+	}
+	
+	public void printVersion() {
+		System.out.println("当前为版本1.0");
 	}
 }
