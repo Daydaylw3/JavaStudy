4552ea4aabf7fb2805ce0ce3d02b5b544ac4e633
谭乐天 <daydaylw3@2980.com>
2019-08-11 07:33:13 +0800

    calculator
    1.0 仅实现了 普通表达式， 未涉及（）的表达式

--- JavaStudy/src/com/dayday/util/Calculator.java
+++ JavaStudy/src/com/dayday/util/Calculator.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Calculator {
 	
 	public static void main(String[] args) {
-		hanoi(20, 'A', 'B', 'C');
+//		hanoi(20, 'A', 'B', 'C');
 //		Node n1 = new Node("A", null),
 //				n2 = new Node("*", null),
 //				n3 = new Node("B", null),
@@ -24,64 +24,70 @@ public class Calculator {
 //		n7.setNext(n8);
 //		n8.setNext(n9);
 //		System.out.println(n1);
-//		System.out.println(getPostfixExp(getInfixExp("a + b * c")));
+		System.out.print(countExp("1*5 +6*9*5+6"));
+//		int[] n = new int[] {
+//				1, 2, 3, 4
+//		};
+//		for (int in : n)
+//			System.out.print(in + " ");
 //		System.out.println();
+//		int i = 1;
+//		n[i - 1] = n[++i] + n[i];
+//		for (int in : n)
+//			System.out.print(in + " ");
 	}
 	
 	private static HashMap<String, Integer> opr = new HashMap<>(8);
 	
 	static {
+		opr.put("(", 5);
+		opr.put("（", 5);
 		opr.put("^", 4);
 		opr.put("*", 3);
 		opr.put("/", 3);
 		opr.put("+", 2);
 		opr.put("-", 2);
+		opr.put(")", 1);
+		opr.put("）", 1);
+	}
+	
+	public static double countExp(String exp) {
+		return countPostfixExp(getPostfixExp(getInfixExp(preDealExp(exp))));
 	}
 	
 	/**
 	 * 计算后缀表达式
 	 * */
-	public static double countPostfixExp(Node head) {
+	private static double countPostfixExp(Node head) {
 		/*
 		 * 1,创建数字栈
 		 * 2,如果为数字,直接入栈
 		 * 3,如果为操作符,将栈顶两个元素出栈,计算后入栈
 		 * */
-		double[] numStack = new double[10];
+		Node[] numStack = new Node[10];
 		int top = -1, size = 0;
-		Node h = head, tmp = head;
+		Node tmp = head;
 		while (tmp != null) {
 			Node next = tmp.next;
+			tmp.next = null;
 			if (!isOpr(tmp)) {
-				numStack[++top] = Double.valueOf(tmp.value);
+				numStack[++top] = tmp;
 				size ++;
 			} else {
-				 
+				numStack[top - 1] = new Node(String.valueOf(countExp(numStack[top - 1], tmp, numStack[top--])), null);
+				size --;
 			}
 			tmp = next;
 		}
-		return 0;
-	}
-	
-	/**
-	 * 将表达式转化为中缀
-	 * */
-	public static Node getInfixExp(String exp) {
-		String[] tmp = exp.split(" ");
-		Node result = new Node(tmp[0], null);
-		Node tmpN = result;
-		for (int i = 1, len = tmp.length; i < len; i++) {
-			Node n = new Node(tmp[i], null);
-			tmpN.next = n;
-			tmpN = tmpN.next;
-		}
-		return result;
+		if (size != 1)
+			throw new RuntimeException("表达式有误");
+		return Double.valueOf(numStack[top].value);
 	}
 	
 	/**
 	 * 将中缀转后缀
 	 * */
-	public static Node getPostfixExp(Node head) {
+	private static Node getPostfixExp(Node head) {
 		// 操作符栈
 		Node[] oprStack = new Node[3];
 		// 指针
@@ -121,10 +127,83 @@ public class Calculator {
 	}
 	
 	/**
+	 * 将表达式转化为中缀
+	 * */
+	private static Node getInfixExp(String exp) {
+		String[] tmp = exp.split(" ");
+		Node result = new Node(tmp[0], null);
+		Node tmpN = result;
+		for (int i = 1, len = tmp.length; i < len; i++) {
+			Node n = new Node(tmp[i], null);
+			tmpN.next = n;
+			tmpN = tmpN.next;
+		}
+		return result;
+	}
+	
+	/**
+	 * 预先处理表达式,使其符合切割规范
+	 * <br>具体为:
+	 * <br>1, 没有连续的两个空格
+	 * <br>2, 数字和操作符中间有空格
+	 * <br>3, "."符号后不能有空格
+	 * 
+	 * @param exp
+	 * @return
+	 */
+	private static String preDealExp(String exp) {
+		// 先去掉所有空格
+		StringBuilder tmp = new StringBuilder(exp), newExp = new StringBuilder();
+		while (tmp.indexOf(" ") >= 0) tmp.deleteCharAt(tmp.indexOf(" "));
+		for (int i = 0; i < tmp.length(); i++) {
+			char c;
+			if (isOpr(c = tmp.charAt(i))) {
+				newExp.append(" " + c + " ");
+			} else {
+				newExp.append(c);
+			}
+		}
+		return newExp.toString();
+	}
+	
+	
+	/**
+	 * 计算表达式
+	 * @param num1
+	 * @param opr
+	 * @param num2
+	 * @return
+	 * @throws IllegalArgumentException 当操作符不在范围内
+	 */
+	private static double countExp(Node num1, Node opr, Node num2) {
+		switch (opr.value) {
+			case "+":
+				return Double.valueOf(num1.value) + Double.valueOf(num2.value);
+			case "-":
+				return Double.valueOf(num1.value) - Double.valueOf(num2.value);
+			case "*":
+				return Double.valueOf(num1.value) * Double.valueOf(num2.value);
+			case "/":
+				return Double.valueOf(num1.value) / Double.valueOf(num2.value);
+			case "^":
+				return Math.pow(Double.valueOf(num1.value), Double.valueOf(num2.value));
+			default:
+				throw new IllegalArgumentException("正确的操作符");
+		}
+	}
+	
+	/**
 	 * 判断是否为操作符
 	 * */
 	private static boolean isOpr(Node n) {
 		return opr.containsKey(n.value);
+	}
+	
+	/**
+	 * 判断是否为操作符
+	 * */
+	private static boolean isOpr(char c) {
+		return opr.containsKey(String.valueOf(c));
 	}
 	
 	/**
